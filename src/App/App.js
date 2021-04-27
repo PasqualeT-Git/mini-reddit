6dca3abc21297c80ad98ac06055a9f6a4871478e
Pasquale Troise <troisepasquale1990@gmail.com>
2021-04-27 16:34:13 +0100

    Change background color of the page relating to the theme-switch

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.css';
 import NavBar from '../features/navBar/NavBar';
+import { getSwitchState } from '../redux/switch/switchSlice';
 
 function App() {
+  const switchState = useSelector(getSwitchState);
+
+  if (switchState) {
+    document.body.style = "background: var(--secondary-dark)"
+  } else {
+    document.body.style = "background: #fff"
+  }
+
   return (
-    <>
+    <div>
       <NavBar />
-    </>
+    </div>
   );
 }
 
