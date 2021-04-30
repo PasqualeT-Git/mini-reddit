a80406cddf9041a6be5d56d3534387cadbf42e5e
Pasquale Troise <troisepasquale1990@gmail.com>
2021-04-30 10:58:01 +0100

    Create a slice to store reddit data, export slice actions and create a reducer for getRedditData

---
+++ src/redux/getRedditData/getRedditDataSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import getRedditDataRequest from '../../helpers/API_requests/getRedditDataRequest'
+
+export const loadPopularSubreddits = createAsyncThunk(
+  'getRedditData/loadPopularSubreddits',
+  (async () => {
+    const jsonResponse = await getRedditDataRequest(`/subreddits/popular`);
+    const topSubredditsArray = jsonResponse.data.children;
+    return topSubredditsArray
+  })()
+)
+
+const initialState = {
+  popularSubreddits: [],
+  isLoadingPopularSubreddits: false,
+  failedToLoadPopularSubreddits: false
+}
+
+const getRedditDataSlice = createSlice({
+  name: 'getRedditData',
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder
+    .addCase(loadPopularSubreddits.pending, (state) => {
+      state.isLoadingPopularSubreddits = true;
+      state.failedToLoadPopularSubreddits = false;
+    })
+    .addCase(loadPopularSubreddits.fulfilled, (state, action) => {
+      state.isLoadingPopularSubreddits = false;
+      state.failedToLoadPopularSubreddits = false;
+      state.popularSubreddits = action.payload;
+    })
+    .addCase(loadPopularSubreddits.rejected, (state) => {
+      state.isLoadingPopularSubreddits = false;
+      state.failedToLoadPopularSubreddits = true;
+    })
+  }
+})
+
+export const isLoadingPopularSubreddits = state => state.getRedditData.isLoadingPopularSubreddits;
+export const selectTopSubreddits = state => state.getRedditData.popularSubreddits;
+
+export default getRedditDataSlice.reducer
