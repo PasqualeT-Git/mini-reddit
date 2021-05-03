3559a89d389441a1b18d8ff63f96fea4caa7e2ed
Pasquale Troise <troisepasquale1990@gmail.com>
2021-05-03 11:33:36 +0100

    Create a Sidebar component that retrieve and show top subreddits, and create its stylesheet

---
+++ src/features/sideBar/SideBar.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import './sideBar.css';
+import { getSwitchState } from'../../redux/switch/switchSlice';
+import {
+  loadPopularSubreddits,
+  selectPopularSubreddits
+} from '../../redux/getRedditData/getRedditDataSlice';
+
+
+const SideBar = () => {
+  const dispatch = useDispatch();
+  const topSubreddits = useSelector(selectPopularSubreddits);
+  const switchStatus = useSelector(getSwitchState);
+
+  useEffect(() =>{
+    dispatch(loadPopularSubreddits())
+  })
+
+  const toggleSideBar = () => {
+    const sidebar = document.querySelector('#sidebar');
+    console.log(sidebar.classList)
+    
+    sidebar.classList.toggle('sidebar_open');
+  }
+
+  return (
+    <div className={switchStatus ? "sidebar_container-dark--closed" : "sidebar_container--closed"} id='sidebar'>
+      <button onClick={toggleSideBar} ><img src={process.env.PUBLIC_URL + '/media/burger-button.svg'} alt="" id="burger_menu"/></button>
+      {topSubreddits.map(topSubreddit => {
+        const subredditData = {
+          id: topSubreddit.data.id,
+          name: topSubreddit.data.display_name_prefixed,
+          icon: topSubreddit.data.icon_img === "" || topSubreddit.data.icon_img === null ? process.env.PUBLIC_URL + 'media/reddit_icon.png' : topSubreddit.data.icon_img,
+          keyColor: topSubreddit.data.key_color,
+          subFullName: topSubreddit.data.name
+        }
+        
+        const {id, name, icon, keyColor} = subredditData;
+
+        return (
+          <li key={id}>
+            <img src={icon} style={{borderColor: keyColor}} id="subreddit_icon" alt=""/>
+            <p>{name}</p>
+          </li>
+        )
+      })}
+    </div>
+  )
+}
+
+export default SideBar;
