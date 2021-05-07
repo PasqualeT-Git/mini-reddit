8d4ddaf12c3e92c6e414da37bf3f7a0562bbbd7f
Pasquale Troise <troisepasquale1990@gmail.com>
2021-05-07 12:19:04 +0100

    Create a loader in CSS and implement it for when isLoadingPosts selector is true

--- src/features/cards/Cards.js
+++ src/features/cards/Cards.js
@@ -4,31 +4,38 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import './cards.css';
 import CardPost from '../card/CardPost';
-import { loadPosts, isLoadingPosts, selectPosts } from '../../redux/getRedditData/postsSlice';
+import { loadPosts, selectPosts, isLoadingPosts } from '../../redux/getRedditData/postsSlice';
 
 const Cards = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
-  
+
   useEffect(() => {
-    dispatch(loadPosts("redditdev"));
+    dispatch(loadPosts('NoStupidQuestions'));
   },[dispatch])
   
   
   return (
     <div className="cards_container">
-      {posts.map(post => {
-        return <CardPost
-          ups={post.data.ups}
-          title={post.data.title}
-          author={post.data.author}
-          content={post.data.selftext && post.data.selftext}
-          comments={post.data.num_comments}
-          date={post.data.created_utc}
-          id={post.data.id}
-          key={post.data.id}
-        />
-      })}
+      {useSelector(isLoadingPosts) ? (
+        <>
+          <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          <p id="loading">Loading posts...</p>
+        </>
+      ) : (
+        posts.map(post => {
+          return <CardPost
+            ups={post.data.ups}
+            title={post.data.title}
+            author={post.data.author}
+            content={post.data.selftext && post.data.selftext}
+            comments={post.data.num_comments}
+            date={post.data.created_utc}
+            id={post.data.id}
+            key={post.data.id}
+          />
+        }))
+      }
     </div>
   )
 }
